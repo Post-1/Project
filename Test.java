b32a4c93f760ce4042572dc37054e627e22efa27
Post-1 <62558632+Post-1@users.noreply.github.com>
2020-04-17 22:36:44 -0500

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test 
-{
-    public static void main(String[] args) {
-        System.out.println("This is just a test program.");
-        System.out.println("The project will be done in Python, not Java.");
-    }
-}
