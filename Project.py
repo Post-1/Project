682b9f9115054a4fe757fac8ded426e1a0c0efe4
Post-1 <62558632+Post-1@users.noreply.github.com>
2020-04-17 22:37:31 -0500

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+
+print(df)#prints out the train.csv file
+print(test)#prints out the test.csv file
